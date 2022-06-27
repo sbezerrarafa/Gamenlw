df3eb00c4a1ec5cc8e14b2ace3071665a913784f
rbezerrajc <rbezerra@sjcc.com.br>
2022-06-27 16:57:03 -0300

    tela de splash

--- gameplay/src/routes/auth.routes.tsx
+++ gameplay/src/routes/auth.routes.tsx
@@ -4,6 +4,7 @@ import {theme} from '../global/styles/themme';
 
 import {Home} from '../screens/Home';
 import {SignIn} from '../screens/SignIn';
+import Splash from '../screens/Splash';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ export function AuthRoutes() {
         },
         headerShown: false,
       }}>
+      <Screen name="Splash" component={Splash} />
       <Screen name="SignIn" component={SignIn} />
       <Screen name="Home" component={Home} />
     </Navigator>
--- gameplay/src/screens/Splash.tsx
+++ gameplay/src/screens/Splash.tsx
@@ -1,38 +1,40 @@
 import React, {Component} from 'react';
-// import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {theme} from '../global/styles/themme';
 
-// interface Props {
-//   navigation: any;
-// }
-// export default class Splash extends React.Component<Props> {
-//   componentDidMount = () => {
-//     setTimeout(() => {
-//       this.props.navigation.navigate('App');
-//     }, 2000);
-//   };
-//   render() {
-//     return (
-//       <View style={style.container}>
-//         <Image source={require('../assets/splash.png')} style={style.image} />
-//         <Text style={style.header}>Game</Text>
-//       </View>
-//     );
-//   }
-// }
+interface Props {
+  navigation: any;
+}
+export default class Splash extends React.Component<Props> {
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.props.navigation.navigate('SignIn');
+    }, 3000);
+  };
+  render() {
+    return (
+      <View style={style.container}>
+        <Image source={require('../assets/splash.png')} style={style.image} />
+        <Text style={style.header}>Game</Text>
+      </View>
+    );
+  }
+}
 
-// const style = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   image: {
-//     height: 200,
-//     width: 200,
-//     resizeMode: 'contain',
-//   },
-//   header: {
-//     fontSize: 50,
-//     fontWeight: 'bold',
-//   },
-// });
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    height: 500,
+    width: 500,
+    resizeMode: 'contain',
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: theme.colors.heading,
+  },
+});
